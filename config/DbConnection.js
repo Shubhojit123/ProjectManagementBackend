4d1a066b6d611cb07a1df59852d8116f7a4c2bd1
Shubhojit saha <139947793+Shubhojit123@users.noreply.github.com>
2025-10-17 00:38:40 +0530

    Update DbConnection.js

--- config/DbConnection.js
+++ config/DbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 exports.dbConnect = async () => {
-  const dbUrl = process.env.DB_URL;
+  const dbUrl = process.env.DB_URL || "mongodb+srv://shubhojit778:Shubhojit%40123@cluster0.iz6rv.mongodb.net/projectmanagement?retryWrites=true&w=majority"
   if (!dbUrl) {
     console.error("DB_URL is not defined in environment variables!");
     return;
